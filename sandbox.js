403a1e3e7f87c46782da53a67e8045b4cddbe6c9
Oğuzhan Beyaz <36895904+oguzbeyaz06@users.noreply.github.com>
2020-08-03 14:59:55 +0300

    Add files

---
+++ sandbox.js
@@ -0,0 +1,32 @@
+const correctAnswer = ['B','B','B','B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let score = 0;
+    const userAnswers =  [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    //console.log(userAnswers);
+    //check answers
+    userAnswers.forEach((answer,index) => {
+        if(answer === correctAnswer[index]){
+            score += 25;
+        }
+    });
+    // show result on page
+
+    
+    result.classList.remove('d-none');
+    scrollTo(0,0);
+    //animation text
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if(output === score){
+            clearInterval(timer);
+        }else {
+            output++;
+        }
+    },10);
+
+});
